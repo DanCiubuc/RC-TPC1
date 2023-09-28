b2fa3fe371a13dbb5b9da67a427d343fc4baba52
dan <dan>
2023-09-28 17:39:35 +0100

    get command

--- TCPclient.py
+++ TCPclient.py
@@ -1,70 +1,135 @@
 import socket
 import sys
 
-msgFromClient       = "Hello UDP Server"
+msgFromClient = "Hello UDP Server"
 
-bytesToSend         = str.encode(msgFromClient)
-
-serverAddressPort   = ("127.0.0.1", 20001)
-bufferSize          = 1024
-serverName          = "localhost"
-
-UDP_Sport = sys.argv[1]    
-server_name = sys.argv[2]
+# TODO:
+# - Fazer vars globais
+# - Deixar os if else mais eficientes e bonitos
+#
 
 
- # Create a UDP socket at client side
+bytesToSend = str.encode(msgFromClient)
+
+bufferSize = 2048
+serverName = "localhost"
+clientPort = 12000
+
+server_name = sys.argv[1]
+
+UDP_Sport = sys.argv[2]
+serverAddressPort = ("127.0.0.1", int(UDP_Sport))
+
+
+# Create a UDP socket at client side
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-while(True):
+while True:
+    args = input()
 
-   args = input()
+    args = args.split(" ")
 
-   args = args.split(" ")
-          
-   command = args[0]
+    command = args[0]
 
-   if command == "open":
-       if len(args) != 2:
-          raise Exception("(1) invalid number of arguments;")
+    if command == "open":
+        if len(args) != 2:
+            raise Exception("(1) invalid number of arguments;")
 
-       bytesToSend = (" ".join(str(element) for element in args)).encode()
+        bytesToSend = (" ".join(str(element) for element in args)).encode()
+        clientPort = int(args[1])
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        response = UDPClientSocket.recvfrom(bufferSize)
+        print(response[0].decode())
 
-   elif command == "close":
+    elif command == "close":
         if len(args) != 1:
             raise Exception("(1) invalid number of arguments;")
         bytesToSend = str(args[0]).encode()
 
-   elif command == "get":
-        print(args)
-   elif command == "put":
-        if len(args) != 2:
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        response = UDPClientSocket.recvfrom(bufferSize)
+        print(response[0].decode())
+
+        UDPClientSocket.close()
+
+        sys.exit("Client closed")
+
+    elif command == "get":
+        if len(args) != 3:
             raise Exception("(1) invalid number of arguments;")
+
+        file_name = args[2]
+
         bytesToSend = (" ".join(str(element) for element in args)).encode()
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-   UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-   response = UDPClientSocket.recvfrom(bufferSize)
-   print(response[0].decode())
+        response = UDPClientSocket.recvfrom(bufferSize)
 
-   if(command == "put" and response[0].decode() == "ack"):
-        #Create TCP Socket
-        TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        TCPClientSocket.connect((serverName, 12000)) #Open TCP connection
+        print(response[0])
 
-        print("TCP Client is running")
+        if response[0].decode() == "ack":
+            # Create TCP Welcoming socket
+            TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            TCPSocket.bind(("", clientPort))
 
-        fileName = args[1]
+            # Begin Listening for incoming TCP Requests
+            TCPSocket.listen(1)
 
-        with open(fileName, "rb") as file:
-            while True:
-                bytesToSend = file.read(bufferSize) #Read bytes in chunks
-                if not bytesToSend:
-                    break #No more bytes to send
-                TCPClientSocket.send(bytesToSend)
-        # Close the client socket
-        TCPClientSocket.close()
+            connSocket, addr = TCPSocket.accept()
 
+            print("TCP Client is running")
 
+            fileName = args[2]
 
-# Send to server using created UDP socket
+            print("Connected by: ", str(addr))  # new socket created on return
+
+            with open(fileName, "wb") as file:
+                while True:
+                    bytesReceived = connSocket.recv(
+                        bufferSize
+                    )  # Receives bytes in chunks
+                    if not bytesReceived:
+                        break  # No more bytes to receive
+                    file.write(bytesReceived)
+            # Close Sockets
+            connSocket.close()
+            TCPSocket.close()
+    elif command == "put":
+        if len(args) != 3:
+            raise Exception("(1) invalid number of arguments;")
+
+        file_name = args[1]
+
+        bytesToSend = (" ".join(str(element) for element in args)).encode()
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+        response = UDPClientSocket.recvfrom(bufferSize)
+
+        print(response[0].decode())
+
+        if response[0].decode() == "ack":
+            # Create TCP Welcoming socket
+            TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            TCPSocket.bind(("", clientPort))
+
+            # Begin Listening for incoming TCP Requests
+            TCPSocket.listen(1)
+
+            connSocket, addr = TCPSocket.accept()
+
+            print("TCP Client is running")
+
+            fileName = args[1]
+
+            print("Connected by: ", str(addr))  # new socket created on return
+
+            with open(fileName, "rb") as file:
+                while True:
+                    bytesToSend = file.read(bufferSize)  # Read bytes in chunks
+                    if not bytesToSend:
+                        break  # No more bytes to send
+                    connSocket.send(bytesToSend)
+            # Close Sockets
+            connSocket.close()
+            TCPSocket.close()
